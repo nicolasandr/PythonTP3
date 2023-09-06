1c426ee0b0fa4814becc46f004f1a0ad9936fae4
nicolas andrade <nicolasandrade328@gmail.com>
2023-09-06 16:31:52 -0300

    agregamos mejoras del punto 1 y 2

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def mostrar_menu():
     print("\nMENU PRINCIPAL:")
     print("\n1_ Crear arreglo de registros")
     print("2_ Cargar por teclado los datos de un ticket")
+    print("3_ leer arreglo")
     print("0_ Para finalizar\n")
 
 
@@ -38,9 +39,10 @@ def crear_arreglo():
         arreglo_registros.append(Ticket(id, patent, tipo_vehiculo, forma_pag, pais_cabina, kilomentros))
 
     file.close()
+    return arreglo_registros
 
 
-def cargar_ticket(linea):
+def cargar_ticket(linea, arreglo_registros):
     id = linea[0:10]
     patent = linea[10:17]
     tipo_vehiculo = linea[17]
@@ -48,33 +50,44 @@ def cargar_ticket(linea):
     pais_cabina = linea[19]
     kilomentros = linea[20:23]
 
-    linea_nueva = str(Ticket(id, patent, tipo_vehiculo, forma_pag, pais_cabina, kilomentros))
+    arreglo_registros.append(Ticket(id, patent, tipo_vehiculo, forma_pag, pais_cabina, kilomentros))
 
-    with open("peajes-tp3.txt", "a") as file:
-        file.write(linea_nueva + "\n")
+    return arreglo_registros
 
 
-def leerarchivo():
-    file = open("peajes-tp3.txt", "rt")
-    for i in file:
+def leerarchivo(arreglo_registros):
+    for i in arreglo_registros:
         print(i)
-    file.close()
 
 
 def Principal():
     opcion = -1
+    arreglo_registros = []
 
     while opcion != 0:
         mostrar_menu()
         opcion = int(input("Ingrese su elección: "))
         if opcion == 1:
-            crear_arreglo()
+            if len(arreglo_registros):
+                while True:
+                    entrada = int(input("esta seguro que desea eliminar el arreglo? (1 = Aceptar / 2 = Cancelar): "))
+                    if entrada == 1 or entrada == 2:
+                        if entrada == 1:
+                            arreglo_registros.clear()
+                            break
+                        if entrada == 2:
+                            break
+                    else:
+                        print("Debes ingresar un valor numérico válido entre(1 y 2).")
+            else:
+                arreglo_registros = crear_arreglo()
+
         elif opcion == 2:
             vector_nuevo_ticket = nuevo_ticket()
             mi_lista = ''.join(vector_nuevo_ticket)
-            cargar_ticket(mi_lista)
+            cargar_ticket(mi_lista, arreglo_registros)
         elif opcion == 3:
-            leerarchivo()
+            leerarchivo(arreglo_registros)
 
 
 if __name__ == '__main__':
